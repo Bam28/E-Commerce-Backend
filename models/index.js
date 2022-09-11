bf9d8ad10dbd8631f993c72a671311dea1d31221
Luis Jimenez <luisjimenez@Luiss-Air.attlocal.net>
2022-09-10 21:27:34 -0500

    Everything is working

--- models/index.js
+++ models/index.js
@@ -14,11 +14,13 @@ Category.hasMany(Product, {
   onDelete: 'CASCADE'
 });
 // Products belongToMany Tags (through ProductTag)
-Product.belongsToMany(ProductTag, {
-  foreignKey: 'tag_id',
+Product.belongsToMany(Tag, {
+  through: ProductTag,
+  foreignKey: 'product_id',
 });
 // Tags belongToMany Products (through ProductTag)
-Tag.belongsToMany(ProductTag, {
+Tag.belongsToMany(Product, {
+  through: ProductTag,
   foreignKey: 'tag_id',
 });
 module.exports = {
